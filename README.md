111fc68387aef39df8520f8990a2934c7f534409
dsaucez <damien.saucez@gmail.com>
2021-08-02 09:45:17 +0000

    number of requests

--- README.md
+++ README.md
@@ -11,5 +11,6 @@ docker run -p 8080:8080 -d -e APISERVER="http://127.0.0.1:3000" frontend
 # Run native
 ```bash
 export APISERVER="http://127.0.0.1:3000"
+export REDISCLUSTER="redis://127.0.0.1:6379"
 npm run start
 ```
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import axios from 'axios';
 import express from 'express';
+import redis from 'redis';
 
-const APISERVER = process.env["APISERVER"]                                                                                                                   
-console.log(APISERVER)
-const PORT = 8080
-const app = express();
+const APISERVER = process.env.APISERVER
+const REDISCLUSTER = process.env.REDISCLUSTER
+console.log("API server: {}\nREDIS cluster: {}".format(APISERVER, REDISCLUSTER))
+
+const PORT = 8080     // TODO should use ENV and fallback to 8080 instead
+const app = express()
+
+const cache = redis.createClient(REDISCLUSTER)
+
+let count = 0;
 
 app.get('/', (req, res) => {
+  // get some statistics
+  cache.get('count',
+            (err, reply) => {
+                              // get the value from cache
+                              if (reply != null) { count = parseInt(reply) }
+                              count = count + 1
+                              // set the new value in cache
+                              cache.set('count', count)
+                            }
+           );
+
+  // API request info
   var config = {
     method: 'GET',
     url: APISERVER + '/time',
@@ -15,11 +34,11 @@ app.get('/', (req, res) => {
 
   axios(config)
     .then(function (response) {
-      const great_text = "Current time is " + response.data.time + " on server " + response.data.id;
+      const great_text = "Current time is {} on server {} (total requests: {})".format(response.data.time, response.data.id, count);
       res.send(great_text);
     })
     .catch(function (error) {
-      res.status(500).send({error:error})
+      res.status(500).send({error:error, count: count})
     });
 })
 app.listen(PORT, () => { console.log('listening on port ' + PORT) })
